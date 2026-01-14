1a027c5b407ba60e66a20adbedd2f2a486bc12d6
Nits-007 <122176311+Nits-007@users.noreply.github.com>
2026-01-14 18:26:21 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -3,16 +3,16 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings) :  #Making class for environment variables
    database_hostname: Optional[str] = None
-    database_port: Optional[str] = None
-    database_password: Optional[str] = None
-    database_name: Optional[str] = None
-    database_username: Optional[str] = None
-    secret_key : str 
-    algorithm : str
-    access_token_expire_minutes : int
+   database_port: Optional[str] = None
+   database_password: Optional[str] = None
+   database_name: Optional[str] = None
+   database_username: Optional[str] = None
+   secret_key : str 
+   algorithm : str
+   access_token_expire_minutes : int
 
-    class Config :
-        env_file = ".env"
+   class Config :
+      env_file = ".env"
 
 settings = Settings() #Instance of the class defined above
  
