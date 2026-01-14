2c12c70942e7c9ee01d9f41c43d5fd0a073311da
Nits-007 <122176311+Nits-007@users.noreply.github.com>
2026-01-14 17:45:19 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,3 +1,30 @@
+# from sqlalchemy import create_engine
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import sessionmaker
+# from app.config import settings
+
+# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:nits007@localhost/fastapi'
+
+# #Not working but it should work as it hides the secrets
+# #SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@"f"{settings.database_hostname }:{settings.database_port}/{settings.database_name}"
+
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL
+# )
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base = declarative_base()
+
+# def get_db(): #ORM=>SQLAlchemy
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+
+
+
+
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -23,4 +50,9 @@ def get_db(): #ORM=>SQLAlchemy
         db.close()
 
 # SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:nits007@localhost/fastapi'
+
+
+#         db.close()
+
+# # SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:nits007@localhost/fastapi'
 
