861e654d27636b1f85cc1cf991ef5193c5f063df
Nits-007 <122176311+Nits-007@users.noreply.github.com>
2026-01-14 17:47:44 +0530

    Enable password hashing and verification functions
    Uncomment password hashing functions for usage.

--- app/utils.py
+++ app/utils.py
@@ -1,3 +1,15 @@
+# from passlib.context import CryptContext
+
+# pwd_context = CryptContext(schemes=["bcrypt"] , deprecated="auto") #Converting password into hash
+
+# def hash(password:str) :
+#     return pwd_context.hash(password)
+
+# def verify(plain_password , hashed_password) :  #Will check whether the password entered during login is same as the password entered during signup
+#     return pwd_context.verify(plain_password , hashed_password)
+
+
+
 from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"] , deprecated="auto") #Converting password into hash
